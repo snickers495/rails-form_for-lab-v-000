ed063d91a4be9955f949cdf74cc1d0a8d7599251
Lisa Lee <snickers495@gmail.com>
2017-12-02 19:15:38 +0000

    automatically backed up by learn

--- app/controllers/schoolclass_controller.rb
+++ app/controllers/schoolclass_controller.rb
@@ -1,14 +1,17 @@
 class SchoolClassController < ApplicationController
   def new
+    @school_class = SchoolClass.new
   end
 
   def show
+    @school_class = SchoolClass.find(params[:id])
   end
 
   def create
   end
 
   def edit
+    @school_class = SchoolClass.find(params[:id])
   end
 
   def update
