8d605e9950f073018dc3e14c0bdabe0608d0b2be
Lisa Lee <snickers495@gmail.com>
2017-12-02 19:22:41 +0000

    automatically backed up by learn

--- app/controllers/schoolclass_controller.rb
+++ app/controllers/schoolclass_controller.rb
@@ -8,7 +8,7 @@ class SchoolClassController < ApplicationController
   end
 
   def create
-    @school_class = SchoolClass.new(post_params(:title, :room_number))
+    @school_class = SchoolClass.new(school_class_params(:title, :room_number))
     @school_class.save
     redirect_to school_class_path(@school_class)
   end
@@ -18,6 +18,8 @@ class SchoolClassController < ApplicationController
   end
 
   def update
+    @school_class = SchoolClass.find(params[:id])
+    @school_class.update(school_class_params(:title, :room))
   end
 
   private
